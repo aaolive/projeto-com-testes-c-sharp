56cb88661f90d3326d2f620684ffe832bb5581b6
Anderson Oliveira <anderson.assis@outlook.com>
2023-09-30 11:51:25 -0300

    Inicio do projeto com testes

---
+++ Calculadora/Program.cs
@@ -0,0 +1,8 @@
+﻿using Calculadora.Services;
+
+CalculadoraCodigo calc =new CalculadoraCodigo();
+
+int valor1 = 12;
+int valor2 = 11;
+
+Console.WriteLine($"Soma do valores {valor1} e {valor2}: {calc.Somar(valor1,valor2 )}");
---
+++ CalculadoraTestes/CalculadoraTestes.cs
@@ -0,0 +1,27 @@
+using Calculadora.Services;
+
+namespace CalculadoraTestes;
+
+public class CalculadoraTestes
+{
+
+    private CalculadoraCodigo calculadora;
+
+    public CalculadoraTestes()
+    {
+        calculadora = new CalculadoraCodigo();
+    }
+
+    [Fact]
+    public void TestaSomaDe12e10()
+    {
+        
+
+        // arrange: cria o teste
+
+        // act
+
+        //assert
+
+    }
+}
